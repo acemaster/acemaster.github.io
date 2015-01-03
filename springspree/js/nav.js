199b2a8ac3023fa2443cfc1c49e88f48aa8e0e03
Vivek Unnikrishnan <vivekhtc25@gmail.com>
2015-01-04 01:03:18 +0530

    changed js file for push menu

--- springspree/js/nav.js
+++ springspree/js/nav.js
@@ -7,18 +7,28 @@
 
 	var body = document.body,
 		mask = document.createElement("div"),
-		togglePushBottom = document.querySelector( ".toggle-push-bottom" ),
-		pushMenuBottom = document.querySelector( ".push-menu-bottom" ),
+		togglePushTop = document.querySelector( ".toggle-push-top" ),
+		//togglePushBottom = document.querySelector( ".toggle-push-bottom" ),
+		pushMenuTop = document.querySelector( ".push-menu-top" ),
+		//pushMenuBottom = document.querySelector( ".push-menu-bottom" ),
 		activeNav
 	;
 	mask.className = "mask";
 
+
+	/* push menu top */
+	togglePushTop.addEventListener( "click", function(){
+		classie.add( body, "pmt-open" );
+		document.body.appendChild(mask);
+		activeNav = "pmt-open";
+	} );
+
 	/* push menu bottom */
-	togglePushBottom.addEventListener( "click", function(){
+	/*togglePushBottom.addEventListener( "click", function(){
 		classie.add( body, "pmb-open" );
 		document.body.appendChild(mask);
 		activeNav = "pmb-open";
-	} );
+	} );*/
 
 	/* hide active menu if mask is clicked */
 	mask.addEventListener( "click", function(){
