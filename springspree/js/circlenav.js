b5ca918461e6ed66075e0fe8ebb520e413c35c39
acemaster <vivekhtc25@gmail.com>
2015-01-31 13:20:04 +0530

    Added circle nav

---
+++ springspree/js/circlenav.js
@@ -0,0 +1,32 @@
+(function () {
+    'use strict';
+    var isOpen  = true;
+    var button  = document.querySelector('#expand-navigation');
+    var wrapper = document.querySelector('.wrapper');
+    var overlay = document.querySelector('.overlay');
+    button.addEventListener('click', navigationHandler);
+    document.addEventListener('click', closeNavigation);
+    function navigationHandler(event) {
+      if (event == null) {
+      event = window.event;
+    }
+    event.stopPropagation();
+    isOpen ? openNavigation() : closeNavigation();
+  }
+
+  function openNavigation() {
+    isOpen = false;
+
+    button.innerHTML  = '-';
+    wrapper.className = 'wrapper opened';
+    overlay.className = 'overlay on-overlay';
+  }
+
+  function closeNavigation() {
+    isOpen = true;
+
+    button.innerHTML = '<img src="img/logotrans.png">';
+    wrapper.className = 'wrapper';
+    overlay.className = 'overlay';
+  }
+})();
