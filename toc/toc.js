3eb817b842f5c49bff9fab8747a9a5d7258e1133
acemaster <vivekhtc25@gmail.com>
2015-12-06 03:49:29 +0530

    Added script for making the state table

--- toc/toc.js
+++ toc/toc.js
@@ -5,13 +5,21 @@ $(document).ready(function(){
             return args[+(a.substr(1,a.length-2))||0];
         });
 };
+	var statearr=[];
+	var variablesarr=[];
 	var no_of_states=1;
 	var no_of_variables=1;
 	var col='<td id="statevariable{0}{1}">{2}</td>';
-	var input='<input type="text" id="variable{0}">';
+	var input='<input type="text" id="variable{0}" placeholder="Next State"><input type="text" id="variablereplace{0}" placeholder="Replacement"><select id="variabledir{0}"><option value="left">Left</option><option value="right">Right</option></select>';
 	var row='<tr id="state{0}">{1}</tr>';
 	function add_state() {
 		htmlstr="";
+		statearr.push($('#newstate').val());
+		console.log(statearr);
+		if(no_of_variables == 1 && no_of_states == 1){
+			variablesarr.push($('#newvariable').val());
+			console.log(variablesarr);
+		}
 		for (var i =1;i<=no_of_variables;i++) {
 			tempstr=input.format(i);
 			htmlstr=htmlstr+col.format(no_of_states,i,tempstr);
@@ -25,6 +33,8 @@ $(document).ready(function(){
 	function add_variable(){
 		htmlstr="";
 		no_of_variables=no_of_variables+1;
+		variablesarr.push($('#newvariable').val());
+		console.log(variablesarr);
 		tempstr=input.format(no_of_variables);
 		for (var i = 1; i <no_of_states; i++) {
 			htmlstr=col.format(i,no_of_variables,tempstr);
